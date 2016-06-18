fe1124b5cc596ad53cf1bee8823b42b79cef97f5
Rachael Serur <rachael@barbershoplabs.com>
2016-06-18 16:43:33 -0400

    Gitignore, webpack merge, webpack validator, webpack-dev-server.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common,  {});
+    break;
+  default:
+    config = merge(common, {});
+}
+
+module.exports = validate(config);
